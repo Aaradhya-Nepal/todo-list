14965f02b4f0a0176e6da4003c4c79ed062d6877
Aaradhya <104313264+Tiresomee@users.noreply.github.com>
2023-01-24 19:02:51 +0545

    second update

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function TodoForm() {
+  const [input, setInput] = useState("");
   return (
-    <div>TodoForm</div>
-  )
+    <form className="todo-form">
+      <input
+        type="text"
+        placeholder="Enter Title"
+        value={input}
+        name="text"
+        className="todo-input"
+      />
+      <br />
+      <button className="todo-button">Add</button>
+    </form>
+  );
 }
 
-export default TodoForm
+export default TodoForm;
