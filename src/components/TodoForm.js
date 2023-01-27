9a8df238d15e4b39be1487fb34f28a58b38e8c24
Aaradhya <104313264+Tiresomee@users.noreply.github.com>
2023-01-27 14:58:29 +0545

    Update the listed tasks

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -4,17 +4,52 @@ import { useState } from "react";
 let nextId = 0;
 
 function TodoForm() {
-  const [inputList, setInputList] = useState("");
-  const [task, setTask] = useState([]);
+  const emptyTask = { id: 0, name: "" };
+  const [task, setTask] = useState(emptyTask);
+  const [tasks, setTasks] = useState([]);
+  const [editMode, setEditMode] = useState(false);
 
   const handleChange = (e) => {
-    setInputList(e.target.value);
+    setTask({
+      ...task,
+      name: e.target.value,
+    });
   };
 
-  const handleClick = () => {
-    setInputList("");
-    setTask([...task, { id: nextId++, inputList: inputList }]);
+  // const getNextId=()=>{
+
+  // }
+
+  const handleAdd = () => {
+    setTasks([...tasks, task]);
+    setTask({
+      ...emptyTask,
+      id: nextId++,
+    });
   };
+
+  const handleDelete = (t) => {
+    setTasks(tasks.filter((a) => a.id !== t.id));
+  };
+
+  const handleEdit = (t) => {
+    setTask(tasks.find((a) => a.id === t.id));
+    setEditMode(true);
+  };
+
+  const handleUpdate = () => {
+    let nextTasks = tasks.filter((a) => a.id !== task.id);
+    setTasks([...nextTasks, task]);
+    handleCancel();
+  };
+
+  const handleCancel = () => {
+    setTask({
+      ...emptyTask,
+      id: nextId,
+    });
+    setEditMode(false);
+  };  
 
   return (
     <>
@@ -22,24 +57,40 @@ function TodoForm() {
         type="text"
         placeholder="Enter Title"
         className="todo-input"
-        value={inputList}
+        value={task.name}
         onChange={handleChange}
       />
-      <button onClick={handleClick}>Add</button>
+      {!editMode ? (
+        <button onClick={handleAdd}>Add</button>
+      ) : (
+        <>
+          <button onClick={handleUpdate}>Update</button>
+          <button onClick={handleCancel}>Cancel</button>
+        </>
+      )}
+
       <ul>
-        {task.map((t) => (
-          <li key={t.id}>
-            {t.inputList}
-            {""}
-            <button
-              onClick={() => {
-                setTask(task.filter((a) => a.id !== t.id));
-              }}
-            >
-              x
-            </button>
-          </li>
-        ))}
+        {tasks &&
+          tasks.map((t) => (
+            <li key={t.id}>
+              {t.name}
+              {""}
+              <button
+                onClick={(e) => {
+                  handleEdit(t);
+                }}
+              >
+                Edit
+              </button>
+              <button
+                onClick={(e) => {
+                  handleDelete(t);
+                }}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
       </ul>
     </>
   );
