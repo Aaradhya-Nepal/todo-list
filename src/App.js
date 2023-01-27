02482beb7e01fddec78e3623fa99c677cf1d80bf
Aaradhya <104313264+Tiresomee@users.noreply.github.com>
2023-01-27 15:57:05 +0545

    Display the Form

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css"; 
 import Banner from "./components/Banner";
 import TodoForm from "./components/TodoForm";
-import TodoList from "./components/TodoList";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
      <div>
       <Banner/>
       <TodoForm/>
-      <TodoList/>
      </div>
     </>
   );
