74e0c19dceda3290503f58ca37bd5bda020f693b
Aaradhya <104313264+Tiresomee@users.noreply.github.com>
2023-01-29 20:56:48 +0545

    Handle nextId

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { useState } from "react";
 
-let nextId = 0;
-
 function TodoForm() {
   const emptyTask = { id: 0, name: "" };
   const [task, setTask] = useState(emptyTask);
@@ -16,25 +14,52 @@ function TodoForm() {
     });
   };
 
-  // const getNextId=()=>{
-
-  // }
+  const getNextId = () => {
+    if (tasks.length > 0) {
+      let maxId = Math.max(...tasks.map((t) => t.id));
+      console.log(maxId);
+      return maxId + 1;
+    } else {
+      return 0;
+    }
+  };
 
   const handleAdd = () => {
-    setTasks([...tasks, task]);
+    let nextId = getNextId();
+    console.log(task);
+    let ts = [...tasks, task];
+    setTasks(ts);
     setTask({
       ...emptyTask,
-      id: nextId++,
+      id: nextId,
     });
+    console.log(nextId);
+    console.log(ts);
   };
 
   const handleDelete = (t) => {
     setTasks(tasks.filter((a) => a.id !== t.id));
+    /*
+    [1,2,3]
+    t = 2
+    1 !== 2 true
+    2 !== 2 false
+    3 !== 2 true
+    [1,3]
+    */
   };
 
   const handleEdit = (t) => {
     setTask(tasks.find((a) => a.id === t.id));
     setEditMode(true);
+    /*
+    [1,2,3]
+    t = 3
+    1 === 3 false
+    2 === 3 false
+    3 === 3 true
+    3
+    */
   };
 
   const handleUpdate = () => {
@@ -46,10 +71,10 @@ function TodoForm() {
   const handleCancel = () => {
     setTask({
       ...emptyTask,
-      id: nextId,
+      id: getNextId(),
     });
     setEditMode(false);
-  };  
+  };
 
   return (
     <>
@@ -74,7 +99,6 @@ function TodoForm() {
           tasks.map((t) => (
             <li key={t.id}>
               {t.name}
-              {""}
               <button
                 onClick={(e) => {
                   handleEdit(t);
